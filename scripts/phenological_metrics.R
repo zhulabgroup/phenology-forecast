67302a6c3db1541f342f3d86eb59017316c3bf3d
yiluansong <yiluansong@gmail.com>
2023-03-31 18:26:54 -0400

    phenological metrics test

--- scripts/phenological_metrics.R
+++ scripts/phenological_metrics.R
@@ -2,17 +2,34 @@
 library(greenbrown)
 
 evi_df_ts <- evi_df %>%
+  filter(id == 1) %>%
+  tidyr::complete(date = seq(min(date), max(date), by = "day")) %>%
   mutate(
     year = as.numeric(format(date, format = "%Y")),
     month = as.numeric(format(date, format = "%m")),
     day = as.numeric(format(date, format = "%d")),
     doy = lubridate::yday(date)
   ) %>%
-  filter(id == 1, year == 2010) %>%
-  tidyr::complete(doy = seq(1, 365, by = 1)) %>%
   filter(doy <= 365) %>%
-  arrange(doy) %>%
   pull(evi) %>%
-  ts(frequency = 365)
+  ts(frequency = 365, start= c(2000,49))
 
-Phenology(evi_df_ts, tsgf = "TSGFspliine", approach = "White")
+res<-Phenology(evi_df_ts, tsgf = "TSGFspline", approach = "White")
+
+# time series pre-processing
+evi_tsgf <- TsPP(evi_df_ts, tsgf = TSGFspline)
+plot(evi_tsgf)
+
+evi_tsgf_df <- evi_tsgf %>% 
+  as_tibble() %>% 
+  rename(evi = x) %>% 
+  bind_cols(data.frame(date = seq(min(evi_df$date), max(evi_df$date), by = "day")) %>% 
+              mutate(doy = lubridate::yday (date),
+                     year = lubridate::year(date)) %>% 
+              filter(doy <= 365) ) 
+  
+  
+ggplot(evi_tsgf_df)+
+  geom_line(aes(x = doy, y= evi, group = year, col=year))+
+  geom_vline(xintercept =  mean(res$sos, na.rm = T)+49-1)+
+  theme_classic()
