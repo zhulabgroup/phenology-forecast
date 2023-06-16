d49f1b48da65dfd4c79591f0209e4af70b76a3ef
ziyuzhou0116 <ziyuzhou@gl3148.arc-ts.umich.edu>
2023-06-16 05:59:15 -0400

    save evi_DB.rds,prep for phenor model

--- scripts/phenological_metrics.R
+++ scripts/phenological_metrics.R
@@ -1,12 +1,13 @@
 #install.packages("greenbrown", repos = "http://R-Forge.R-project.org", dependencies = FALSE)
 library(greenbrown)
 
+# extract phenological metrics
 df_metric <- evi_all <- data.frame()
-df_metric <- data.frame(id=1:12,sos = NA, eos = NA)
+df_metric <- data.frame(id=1:100,sos = NA, eos = NA)
+sos_matrix <- matrix(NA,nrow = 100,ncol = 23)
 
-for (i in 1:12){
-  evi_ts <- evi_df %>%
-    filter(id == 12) %>%
+for (i in 1:100){
+  evi_ts <- evi_df %>% filter(id == i) %>%
     tidyr::complete(date = seq(min(date), max(date), by = "day")) %>%
     mutate(
       year = as.numeric(format(date, format = "%Y")),
@@ -24,28 +25,37 @@ for (i in 1:12){
   
   res<-Phenology(evi_ts, tsgf = "TSGFspline", approach = "White")
   
-  df_metric$sos[1] <- mean(res$sos,na.rm = T)+49-1
-  df_metric$eos[1] <- mean(res$eos,na.rm = T)+49-1
-  
+  df_metric$sos[i] <- mean(res$sos,na.rm = T)+49-1
+  df_metric$eos[i] <- mean(res$eos,na.rm = T)+49-1
+  sos <- res$sos + 49-1
+  eos <- res$eos + 49-1
+  for(year in 2000:2022){
+    sos_value = sos
+    sos_matrix[i,year-2000+1] <- sos_value[year-1999]
+
+  }
   
   # time series pre-processing
   evi_tsgf <- TsPP(evi_ts, tsgf = TSGFspline)
-  tsgf1 <- TSGFdoublelog(window(evi_ts,2000,c(2022,365)),interpolate = T,method = "Elmore", 
-                         check.seasonality = NULL)
-  plot(evi_tsgf)
-  lines(tsgf1,col="blue")
   
-  evi_tsgf_df <- tsgf1 %>% 
+  # logistic approach
+  #tsgf1 <- TSGFdoublelog(window(evi_ts,2000,c(2022,365)),interpolate = T,method = "Elmore", 
+                         #check.seasonality = NULL)
+  plot(evi_tsgf)
+  lines(evi_tsgf,col="blue")
+  
+  evi_tsgf_df <- evi_tsgf %>% 
     as_tibble() %>% 
     rename(evi = x) %>% 
     bind_cols(data.frame(date = seq(min(evi_df$date), max(evi_df$date), 
                                     by = "day")) %>% 
                 mutate(doy = lubridate::yday (date),
                        year = lubridate::year(date),
-                       id = 1) %>% filter(year <= 2022) %>%
+                       id = i) %>% filter(year <= 2022) %>%
                 filter(doy <= 365) ) 
   evi_all <- bind_rows(evi_all,evi_tsgf_df)
-  
+  colnames(sos_matrix) <- 2000:2022
+  rownames(sos_matrix) <- unique(evi_df$id)
   
   
   
@@ -61,4 +71,98 @@ ggplot()+
   geom_vline(data = df_metric,
              aes(xintercept = eos))+
   theme_classic()+ facet_wrap(.~id)
+
+# download daymet and reform evi_DB structure
+evi_DB <- list()
+for (i in evi_df[1:100,]$id){
+  df_daymet <- daymetr::download_daymet(
+    site = evi_df$id[i],
+    lat = evi_df$lat[i],
+    lon = evi_df$lon[i],
+    start = lubridate::year(min(evi_df$date)),
+    end = min(lubridate::year(max(evi_df$date))-1),
+    internal = TRUE,
+    simplify = TRUE
+  ) %>%
+    filter(measurement %in% c("dayl..s.","tmax..deg.c.", "tmin..deg.c.", "prcp..mm.day.", "vp..Pa.")) %>%
+    spread(key = "measurement", value = "value") %>%
+    rename(
+      dayl = `dayl..s.`,
+      prcp = `prcp..mm.day.`,
+      tmax = `tmax..deg.c.`,
+      tmin = `tmin..deg.c.`,
+      vp = `vp..Pa.`
+    ) %>%
+    mutate(date = as.Date(yday, origin = paste0(year, "-01-01")) - 1) %>%
+    mutate(temp = (tmax + tmin / 2)) %>%
+    mutate(dayl = dayl / 3600) %>% as_tibble()  %>%
+    mutate(year = lubridate::year(date))
+  
+  df_daymet <- df_daymet[c("site", "date", "dayl","year", "temp", "tmax", "tmin","prcp", "vp")]
+  evi_daymet <- evi_all %>% filter(id == i) %>% left_join(df_daymet, by = c("date","year"))
+  evi_daymet <- as_tibble(evi_daymet)
+  
+  evi_temp <- evi_daymet %>% arrange(doy) %>% filter(site == i)
+  evi_temp <- evi_temp[c("year","doy","temp")] %>% 
+    pivot_wider(names_from = year,values_from = temp)
+  evi_temp <- evi_temp[2:23]
+  
+  evi_ltm <- evi_daymet %>% arrange(doy) %>% filter(site == i) 
+  evi_ltm <- evi_ltm[c("year","doy","temp")] %>%
+    pivot_wider(names_from = year,values_from = temp) 
+  evi_ltm <- evi_ltm[2:23] %>%
+    mutate(ltm = rowMeans(., na.rm = TRUE))
+  
+  evi_tmin <- evi_daymet %>% arrange(doy) %>% filter(site == i) 
+  evi_tmin <- evi_tmin[c("year","doy","tmin")] %>%
+    pivot_wider(names_from = year,values_from = tmin) 
+  evi_tmin <- evi_tmin[2:23]
+  
+  evi_tmax <- evi_daymet %>% arrange(doy) %>% filter(site == i) 
+  evi_tmax <- evi_tmax[c("year","doy","tmax")] %>%
+    pivot_wider(names_from = year,values_from = tmax) 
+  evi_tmax <- evi_tmax[2:23]
+  
+  evi_prcp <- evi_daymet %>% arrange(doy) %>% filter(site == i) 
+  evi_prcp <- evi_prcp[c("year","doy","prcp")] %>%
+    pivot_wider(names_from = year,values_from = prcp) 
+  evi_prcp <- evi_prcp[2:23]
+  
+  evi_vp <- evi_daymet %>% arrange(doy) %>% filter(site == i) 
+  evi_vp <- evi_vp[c("year","doy","vp")] %>%
+    pivot_wider(names_from = year,values_from = vp) 
+  evi_vp <- evi_vp[2:23]
+  
+  evi_dayl <- evi_daymet %>% arrange(doy) %>% filter(site == i) 
+  evi_dayl <- evi_dayl[c("year","doy","dayl")] %>%
+    pivot_wider(names_from = year,values_from = dayl) 
+  evi_dayl <- evi_dayl
+  
+  
+  
+  site_list <- list()
+  site_list[["site"]] <- unique(evi_daymet$site)
+  site_list[["location"]] <- c(evi_df$lon[i],evi_df$lat[i])
+  site_list[["doy"]] <- sort(unique(evi_daymet$doy))
+  site_list[["ltm"]] <- evi_ltm$ltm
+  site_list[["sos"]] <- unname(round(sos_matrix[1,])) 
+  site_list[["year"]] <- unique(evi_daymet$year)
+  site_list[["Ti"]] <- evi_temp
+  site_list[["Tmini"]] <- evi_tmin
+  site_list[["Tmaxi"]] <- evi_tmax
+  site_list[["Li"]] <- evi_dayl
+  site_list[["Pi"]] <- evi_prcp
+  site_list[["VPDi"]] <- evi_vp
+  
+  
+  name <- as.character(i)
+  evi_DB[[name]] <- site_list
+  
+}
+View(evi_temp)
+
+View(evi_DB)
+write_rds(evi_DB,"evi_DB.rds")
+
+
 
--- scripts/phenor_model_compare.R
+++ scripts/phenor_model_compare.R
@@ -1,48 +1,176 @@
-library(tidyverse)
 library(phenor)
+library(greenbrown)
+library(tidyverse)
+
+# load the included data using
 data("phenocam_DB")
+View(phenocam_DB)
+# comma separated parameter file inlcuded in the package
+# for all the included models, this file is used by default
+# in all optimization routines
 path <- sprintf("%s/extdata/parameter_ranges.csv",path.package("phenor"))
-path_ranges <- read.table(path,
-                          header=TRUE,
-                          sep=",")
+par_ranges <- read.table(path,
+                         header = TRUE,
+                         sep = ",")
 
+# optimize model parameters
 set.seed(1234)
-
-optim.par <- optimize_parameters(par = c(180,0,250,500,2.5),
-                               data = phenocam_DB,
+optim.par <- pr_fit_parameters(par = c(180,180,0,4,2.5,7.5,1000,180),
+                               data = evi_DB,
                                cost = rmse,
-                               model = "AT",
+                               model = "SQ",
                                method = "GenSA",
-                               lower = c(1,-10,0,0,0),
-                               upper = c(365,10,500,1000,5),
+                               lower = c(1,1,-10,-5,-5,0,0,0),
+                               upper = c(365,365,10,10,10,15,2000,350),
                                control=NULL)
-modelled <- estimate_phenology(data = phenocam_DB,
-                               par = optim.par$par, model = "AT")
-df_list <- vector(mode = "list")
-for(s in names(phenocam_DB)){
-  df_list[[s]] <- data.frame(site = s,
-                             year=phenocam_DB[[s]]$year,
-                             doy = phenocam_DB[[s]]$transition_dates)
+optim.par
+?pr_fit_parameters
+
+#SQ
+c(180,180,0,2.5,2.5,7.5,1000,180)
+c(1,1,-10,-5,-5,0,0,0)
+c(365,365,10,10,10,15,2000,350)
+
+#AT
+c(180,0,250,500,2.5)
+c(1,-10,0,0,0)
+c(365,10,500,1000,5)
+
+#PA
+c(180,180,0,2.5,2.5,7.5,0.5,1000,175)
+c(1,1,-10,-5,-5,0,0,0,0)
+c(365,365,10,10,10,15,1,2000,350)
+
+#UN
+c(180,5,5,-10,-10,7.5,0,50,175)
+c(1,0,0.1,-20,-20,-5,-10,0,0)
+c(365,10,10,0.1,0.1,20,10,100,350)
+
+#SM1
+c(180,180,0,2.5,2.5,7.5,1000,175)
+c(1,1,-10,-5,-5,0,0,0)
+c(365,365,10,10,10,15,2000,350)
+
+#PM1
+c(180,0,2.5,2.5,7.5,0.5,1000,175)
+c(1,-10,-5,-5,0,0,0,0)
+c(365,10,10,10,15,1,2000,350)
+
+#SGSI
+c(-7.5,22.5,0.5,1000,3500,10,11)
+c(-15,0,0,0,2000,8,10)
+c(0,45,1,2000,5000,11.5,12)
+
+
+# now run the model for all data in the nested list using the estimated parameters
+modelled <- pr_predict(data = phenocam_DB, par = optim.par$par,model = "LIN")
+
+# compare predictions with observations
+df_list<-vector(mode="list")
+for (s in names(phenocam_DB)) {
+  df_list[[s]]<-data.frame(site=s,
+                           year=phenocam_DB[[s]]$year,
+                           doy=phenocam_DB[[s]]$transition_dates)
+  
 }
 
-df <- bind_rows(df_list)
-df_AT <- df %>% mutate(pred = modelled) %>% 
-  mutate(rmse = sqrt(mean((doy - pred)^2))) %>%
-  mutate(model = "AT")
-
-View(df_PTT)
+df<-bind_rows(df_list)
+df_TT<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "TT") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
 View(df_TT)
+
+df_PTT<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "PTT") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_PTT)
+
+df_AT<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "AT") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
 View(df_AT)
-ggplot(df) + geom_jitter(aes(x=doy,y=pred,col=site)) + 
-  theme_classic() + 
-  guides(col="none") +
-  facet_wrap(.~model)
+
+df_SQ<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "SQ") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_SQ)
+
+df_M1<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "M1") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_M1)
+
+df_PA<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "PA") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_PA)
+
+df_SM1<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "SM1") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_SM1)
+
+df_PM1<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "PM1") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_PM1)
+
+df_SGSI<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "SGSI") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_SGSI)
+
+df_LIN<-df %>% 
+  mutate(pred=modelled) %>% mutate(model = "LIN") %>%
+  mutate(rmse = sqrt(mean((doy-pred)^2)))
+View(df_LIN)
+
+
+df_com <- rbind(df_LIN,df_TT,df_PTT,df_M1,df_AT,df_SQ,df_PA,df_SM1,df_PM1)
+View(df_com)
+
+# Reorder the levels of the model variable in your data frame
+df_com$model <- factor(df_com$model, 
+                       levels = c("LIN", "TT", "PTT", "M1","AT","SQ","SM1","PA","PM1"))
+
+
+ggplot(df_com)+
+  geom_jitter(aes(x=doy, y=pred, col=site))+
+  theme_classic()+
+  guides(col="none")+
+  facet_wrap(.~model) + ylab("pred doy")
+
+ggplot(df_com) + geom_boxplot(aes(x = model,y = rmse,color = model)) + 
+  theme_classic() +
+  ylim(0,20) + 
+  scale_x_discrete(limits = c("LIN", "TT", "PTT", "M1","AT","SQ","SM1","PA","PM1")) +
+  scale_color_manual(values = c("LIN" = "black", "TT" = "orange", 
+                                "PTT" = "orange", "M1" = "orange",
+                                "AT" = "blue","SQ" = "blue",
+                                "SM1" = "blue","PA" = "blue",
+                                "PM1" = "blue")) +  ylab("RMSE (days)") + xlab("Models") +
+  guides(color = FALSE)
 
 
 
+data(ndvi)
+x <- as.vector(window(ndvi, start=c(1991,1), end=c(1991, 12)))
+plot(x)
 
+# fit double-logistic function to one year of data
+fit <- FitDoubleLogElmore(x)
+fit
+plot(x)
+lines(fit$predicted, col="blue")
 
+# do more inital trials, plot iterations and compute parameter uncertainties
+FitDoubleLogElmore(x, hessian=TRUE, plot=TRUE, ninit=1000)	
 
-
-
+# fit double-logistic function to one year of data, 
+# interpolate to daily time steps and calculate phenology metrics
+tout <- seq(1, 12, length=365)	# time steps for output (daily)
+fit <- FitDoubleLogElmore(x, tout=tout)
+plot(x)
+lines(tout, fit$predicted, col="blue")
+PhenoDeriv(fit$predicted, plot=TRUE)
 
